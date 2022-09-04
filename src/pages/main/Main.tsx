dff502c9126bc54dbfb29fb87e17f0a5c116672d
Dzmitry Paulau <dmitriy.pavlov.17.04@gmail.com>
2022-09-04 12:53:35 +0300

    fix: fix a bug when you go at once to the page with cards

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
+import getCategories from '../../api/categories';
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
+import { setCategories } from '../../store/slice';
 import { State } from '../../types/store';
 import styles from './main.module.css';
 
 function Main() {
   const isNavbarShown = useSelector((state: State) => state.isNavbarShown);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getCategories().then((categories) => dispatch(setCategories(categories)));
+  }, []);
+
   return (
     <div className={styles.page}>
       <div

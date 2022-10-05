84af171f6cf2220cf1f6ad1f6221a9923b5f5c07
Dzmitry Paulau <dmitriy.pavlov.17.04@gmail.com>
2022-10-05 16:38:58 +0300

    feat: update visual of app title

--- src/components/main-page/header/Header.tsx
+++ src/components/main-page/header/Header.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store/store';
 import Hamburger from '../../../UI/hamburger/Hamburger';
 import Switch from '../../../UI/switch/Switch';
 import styles from './header.module.css';
 
 function Header() {
+  const isPlayMode = useSelector((state: RootState) => state.common.isPlayMode);
+
   return (
     <header className={styles.header}>
       <Hamburger />
-      <h1 className={styles.title}>English for Kids</h1>
+      <h1
+        className={isPlayMode ? `${styles.title} ${styles.play}` : styles.title}
+      >
+        English for Kids
+      </h1>
       <Switch />
     </header>
   );
